a48de706b52e5461aa8e1728516327ff809db0e5
cjp <cjp_ca@163.com>
2021-01-02 23:06:18 +0800

    add gitignore

---
+++ common_test/common_test/common_test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace common_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
